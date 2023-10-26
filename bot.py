2a75ac47ed412dc315304f8601818196220dc240
sridevkp <69075577+sridevkp@users.noreply.github.com>
2023-10-26 23:04:08 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+API_KEY = os.get_env('API_KEY')
+USERNAME = 'guudMorning_bot'
+
+#commands
+async def start_command( update, context ):
+    update.message.reply_text("Hello")
+
+async def help_command( update, context ):
+    update.message.reply_text("Help is here")
+
+
+async def custom_command( update, context ):
+    update.message.reply_text("custom?")
+
+#respond
+
+
+async def handle_message( update, context ):
+    msg_type = update.message.chat.type #group/private
+    text = update.message.text
+
+    if msg_type == "group":
+        return
+
+async def error( update, context ):
+    print( update, context)
+
+
+if __name__ == '__main__' :
+    app = Application.builder().token(API_KEY).build()
+    #add command handlers
+    app.add_handler( CommandHandler( 'start' , start_command ))
+    app.add_handler( CommandHandler( 'help'  , start_command ))
+    app.add_handler( CommandHandler( 'custom', start_command ))
+    #add message handler
+    app.add_handler( MessageHandler( filters.TEXT, handle_message ))
