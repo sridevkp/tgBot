843c7b1d234c6bd461204b56c97bdfd2b89d5e2b
sridevkp <69075577+sridevkp@users.noreply.github.com>
2023-11-30 22:19:34 +0530

    safety first

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ def get_file(url, json=False):
 
 
 if __name__ == '__main__' :
-    app = Application.builder().token("6774887793:AAEVdb6lBt-fXd5aPYviyt_O7rv_roqEcf8").build()
+    app = Application.builder().token(TOKEN).build()
 
     #add command handlers
     app.add_handler( CommandHandler( 'start' , start_command  ))
